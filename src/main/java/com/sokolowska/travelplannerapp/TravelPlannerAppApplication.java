d0260f28c84917f6dceb64c4851a81d4c77da3cc
Joanna Sokolowska <joanna_s@op.pl>
2022-06-10 10:37:25 +0200

    initial commit

---
+++ src/main/java/com/sokolowska/travelplannerapp/TravelPlannerAppApplication.java
@@ -0,0 +1,13 @@
+package com.sokolowska.travelplannerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPlannerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelPlannerAppApplication.class, args);
+    }
+
+}
